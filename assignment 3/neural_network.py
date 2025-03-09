1728474e14fa90c95cde949389b27aac8df2c38c
skyfall1818 <phid8181@hotmail.com>
2025-03-09 00:52:39 -0500

    attributes, and training text update
    the program now is capable of taking the attribute and train text files and creating a neural network based on attribute inputs and target outputs. works on tennis, iris, and identity

--- assignment 3/neural_network.py
+++ assignment 3/neural_network.py
@@ -1,7 +1,15 @@
+import sys
 import random
 import math
 
-INPUT_NODES = 1
+LEARNING_RATE = 0.1
+MOMENTUM_CONSTANT = 0.9
+NUM_ITERATIONS = 20
+NUM_INPUT_NODES = 2
+NUM_HIDDEN_NODES = 2
+NUM_OUTPUT_NODES = 1
+ATTRIBUTES = {}
+TARGET = []
 
 class Node:
     def __init__(self, sigmoid):
@@ -72,10 +80,171 @@ class Network:
 
         deltaError = [(expectedOut[i] - out[i].get_val()) * out[i].getDerivative() for i in range(self.numOutputNodes)]
 
+        # backpropogation to the hidden nodes
         deltahidden = [0 for _ in range(self.numHiddenNodes + 1)]
         for i in range(self.numHiddenNodes + 1):
             deltahidden[i] = self.hiddenNodes[i].getDerivative() * sum([deltaError[j] * self.hiddenWeights[i][j] for j in range(self.numOutputNodes)])
 
         for i in range(self.numHiddenNodes + 1):
             for j in range(self.numOutputNodes):
-                self.hiddenWeights[i][j] += self.learningRate * deltahidden[j] * self.hiddenNodes[i].get_val()
+                if i == self.numHiddenNodes:
+                    self.hiddenWeights[i][j] += self.learningRate * deltahidden[j] * self.hiddenNodes[i].get_val()
+                else: 
+                    self.hiddenWeights[i][j] += self.learningRate * deltahidden[j] # * 1  constant w0
+
+        # repeat backprogogation to the input node
+        deltaInput = [0 for _ in range(self.numInputNodes + 1)]
+        for i in range(self.numInputNodes + 1):
+            deltaInput[i] = self.inputWeights[i].getDerivative() * sum([deltahidden[j] * self.inputWeights[i][j] for j in range(self.numHiddenNodes + 1)])
+        
+        for i in range(self.numInputNodes + 1):
+            for j in range(self.numHiddenNodes + 1):
+                if i == self.numInputNodes:
+                    self.inputWeights[i][j] += self.learningRate * deltaInput[j] * self.hiddenNodes[j].get_val()
+                else:
+                    self.inputWeights[i][j] += self.learningRate * deltaInput[j] # * 1  constant w0
+    
+    def output_nodes_and_weights(self):
+        hiddenLength = 0
+        for i in range(self.numInputNodes + 1):
+            txt = str(["{:.1f}".format(i) for i in self.inputWeights[i]])
+            if i < self.numHiddenNodes:
+                txt += "  [" + "{:.1f}".format(self.hiddenNodes[i].get_val()) + "]  "
+            else:
+                txt += "         "
+            if i < self.numHiddenNodes + 1:
+                hiddenStr = str(["{:.1f}".format(i) for i in self.hiddenWeights[i]])
+                if len(hiddenStr) > hiddenLength:
+                    hiddenLength = len(hiddenStr)
+                txt += hiddenStr
+            else:
+                txt += (" " * hiddenLength)
+            
+            if i < self.numOutputNodes:
+                txt += "  [" + "{:.1f}".format(self.outputNodes[i].get_val()) + "]"
+            print(txt)
+
+def read_settings_file(file):
+    global LEARNING_RATE, MOMENTUM_CONSTANT, NUM_ITERATIONS, NUM_HIDDEN_NODES
+    with open(file, "r") as f:
+        for line in f.readlines():
+            if '=' not in line:
+                continue
+            name,val = line.split("=")
+            val  = val.strip()
+            if 'Hidden' in name:
+                NUM_HIDDEN_NODES = int(val)
+            elif 'Learning' in name:
+                LEARNING_RATE = float(val)
+            elif 'Momentum' in name:
+                MOMENTUM_CONSTANT =float(val)
+            elif 'Iterations' in name:
+                NUM_ITERATIONS = int(val)
+
+
+def read_attributes_file(file):
+    global NUM_INPUT_NODES, NUM_OUTPUT_NODES, ATTRIBUTES, TARGET
+    target = False
+    first = True
+    cnt = 0
+    with open(file, "r") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.strip() == '':
+                target = True
+                continue
+            if not target:
+                att = line.split()
+                if ':' in att[0]:
+                    cnt += 1
+                    ATTRIBUTES[att[0]] = ['binary']
+                else:
+                    cnt += len(att) - 1
+                    ATTRIBUTES[att[0]] = att[1:]
+            else:
+                if first and len(lines) == i + 1:
+                    TARGET = [i.strip() for i in line.split()[1:]]
+                else:
+                    first = False
+                    TARGET.append( [i.strip() for i in line.split()[1:]])
+    NUM_OUTPUT_NODES = len(TARGET)
+    NUM_INPUT_NODES = cnt
+
+def read_data_file(file):
+    global ATTRIBUTES
+    dataList = []
+    attList = list(ATTRIBUTES.keys())
+    with open(file, "r") as f:
+        for line in f.readlines():
+            if '  ' in line:
+                data = {}
+                instance = []
+                inputs, outputs = line.split('  ')
+                print(inputs)
+                for i, val in enumerate(inputs.split()):
+                    if len (ATTRIBUTES[attList[i]]) == 1:
+                        instance.append(val)
+                    else:
+                        attVals = [ 0 for _ in range(len(ATTRIBUTES[attList[i]]))]
+                        for i, item in enumerate(ATTRIBUTES[attList[i]]):
+                            if item in val:
+                                attVals[i] = 1
+                                break
+                        instance.extend(attVals)
+                data['attributes'] = instance
+                data['target'] = outputs.split()
+                dataList.append(data)
+            else:
+                data = {}
+                instance = []
+                inputs = line.split()
+                for i, val in enumerate(inputs):
+                    if i+1 == len(inputs):
+                        break
+                    elif len (ATTRIBUTES[attList[i]]) == 1:
+                        instance.append(val)
+                    else:
+                        attVals = [ 0 for _ in range(len(ATTRIBUTES[attList[i]]))]
+                        for i, item in enumerate(ATTRIBUTES[attList[i]]):
+                            if item in val:
+                                attVals[i] = 1
+                                break
+                        instance.extend(attVals)
+                data['attributes'] = instance
+                if ':' in inputs[-1]:
+                    data['target'] = 'binary'
+                else:
+                    data['target'] = inputs[-1]
+                dataList.append(data)
+    return dataList
+
+if __name__ == "__main__":
+
+    if len(sys.argv) <= 1:
+        print("No arguments provided.")
+        exit(-1)
+    
+    settingsFile = sys.argv[1]
+    read_settings_file(settingsFile)
+    print('Number of Hiden Nodes: ', NUM_HIDDEN_NODES)
+    print('Learning Rate: ', LEARNING_RATE)
+    print('Momentum Constant: ', MOMENTUM_CONSTANT)
+    print('Number of Iterations: ', NUM_ITERATIONS)
+
+    attributeFile = sys.argv[2]
+    read_attributes_file(attributeFile)
+    print(ATTRIBUTES)
+    print('Number of Input Nodes: ', NUM_INPUT_NODES)
+    print('Number of Output Nodes: ', NUM_OUTPUT_NODES)
+    print('Target: ', TARGET)
+
+    dataFile = sys.argv[3]
+    trainingSet = read_data_file(dataFile)
+    for data in trainingSet:
+        print(data)
+
+    net = Network(NUM_INPUT_NODES, NUM_HIDDEN_NODES, NUM_OUTPUT_NODES, LEARNING_RATE, MOMENTUM_CONSTANT)
+    net.output_nodes_and_weights()  
+
+    
+    
